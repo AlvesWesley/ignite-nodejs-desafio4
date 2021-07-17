0eaf6ca558c3bb7299d8b63a99263d2e9ae7da10
Wesley Alves <wesley_alv@outlook.com>
2021-07-17 14:39:26 -0300

    concluindo desafio

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,13 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user.admin) {
+      throw new Error("You must to be an admin to list users");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
